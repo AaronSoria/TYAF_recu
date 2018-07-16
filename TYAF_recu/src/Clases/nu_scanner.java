a22a27d363849b7bbbe82f7e2f29ed99a944b18f
Aaron Soria <stark28@JARVIS>
2018-07-16 02:29:01 -0300

    el escaner de objetos ahora es mas exigente

--- TYAF_recu/src/Clases/nu_scanner.java
+++ TYAF_recu/src/Clases/nu_scanner.java
@@ -22,6 +22,16 @@ public class nu_scanner {
 public int full;
 public int lower;
 public int upper;
+public int breasts;
+
+
+    public void reset_flags()
+    {
+        this.full=0;
+        this.lower=0;
+        this.upper=0;
+        this.breasts=0;
+    }
     
     public void convert_to_hsv(String origin_paht, String destiny_paht)
     {
@@ -35,6 +45,22 @@ public int upper;
             Mat b = new Mat(1, 1, CV_32SC4, new org.bytedeco.javacpp.IntPointer(35,174,255,0));
             org.bytedeco.javacpp.opencv_core.inRange(hsv,a,b,skin);
             org.bytedeco.javacpp.opencv_imgcodecs.imwrite(destiny_paht, skin);
+        }
+        catch(Exception ex)
+        {
+            System.out.println("error con: " + origin_paht);
+        }
+        
+    }
+    
+    public void convert_to_greyscale(String origin_paht, String destiny_paht)
+    {
+        try
+        {
+            Mat bgr = org.bytedeco.javacpp.opencv_imgcodecs.imread(origin_paht);
+            Mat greyscale = new Mat();
+            org.bytedeco.javacpp.opencv_imgproc.cvtColor(bgr, greyscale, org.bytedeco.javacpp.opencv_imgproc.COLOR_BGR2GRAY);
+            org.bytedeco.javacpp.opencv_imgcodecs.imwrite(destiny_paht, greyscale);
         }
         catch(Exception ex)
         {
@@ -98,27 +124,6 @@ public int upper;
         catch (IOException ex){return -1;}
     }
     
-    public void body_detection()
-    {
-        
-    }
-    
-//    public void full_body_detection(String src) //funciona
-//    {
-//        try
-//        {
-//            IplImage img = cvLoadImage(src);
-//            String XML_FILE = "XML/haarcascade_fullbody.xml";
-//            CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(XML_FILE));
-//            CvMemStorage storage = CvMemStorage.create();
-//            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.05,3,CV_HAAR_DO_CANNY_PRUNING);
-//            cvClearMemStorage(storage);
-//            int total_bodies = sign.total(); 
-//            System.out.println(src +": " + total_bodies);
-//        }
-//        catch(Exception e){}
-//    }
-
     public void full_body_detection(String src) //funciona
     {
         try
@@ -127,7 +132,7 @@ public int upper;
             String XML_FILE = "XML/haarcascade_fullbody.xml";
             CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(XML_FILE));
             CvMemStorage storage = CvMemStorage.create();
-            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.05,3,CV_HAAR_DO_CANNY_PRUNING);
+            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.8,5,CV_HAAR_DO_CANNY_PRUNING);
             cvClearMemStorage(storage);
             this.full= sign.total();
 //            for(int i = 0; i < this.full; i++)
@@ -159,7 +164,7 @@ public int upper;
             String XML_FILE = "XML/haarcascade_lowerbody.xml";
             CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(XML_FILE));
             CvMemStorage storage = CvMemStorage.create();
-            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.05,3,CV_HAAR_DO_CANNY_PRUNING);
+            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.8,5,CV_HAAR_DO_CANNY_PRUNING);
             cvClearMemStorage(storage);
             this.lower = sign.total();
 //            for(int i = 0; i < this.lower; i++)
@@ -191,7 +196,7 @@ public int upper;
             String XML_FILE = "XML/haarcascade_upperbody.xml";
             CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(XML_FILE));
             CvMemStorage storage = CvMemStorage.create();
-            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.05,3,CV_HAAR_DO_CANNY_PRUNING);
+            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.8,5,CV_HAAR_DO_CANNY_PRUNING);
             cvClearMemStorage(storage);
             this.upper = sign.total();
 //            for(int i = 0; i < this.upper; i++)
@@ -212,6 +217,39 @@ public int upper;
         catch(Exception e)
         {
             
+        }
+    }
+    
+    
+    public void breasts_detection(String src) //funciona
+    {
+        try
+        {
+            IplImage img = cvLoadImage(src);
+            String XML_FILE = "XML/cascade.xml";
+            CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(XML_FILE));
+            CvMemStorage storage = CvMemStorage.create();
+            CvSeq sign = cvHaarDetectObjects(img,cascade,storage,1.45,3,CV_HAAR_DO_CANNY_PRUNING);
+            cvClearMemStorage(storage);
+            this.breasts = sign.total();
+//            for(int i = 0; i < this.upper; i++)
+//                {
+//                    System.out.println(".");
+//                    CvRect faceRect = new CvRect(cvGetSeqElem(sign, i));
+//                    cvRectangle(img, 
+//                                cvPoint(faceRect.x(), faceRect.y()), 
+//                                cvPoint(faceRect.x(), faceRect.y()), 
+//                                CV_RGB(255,0, 0), 3, 8, 0);
+//                }
+//            if (this.upper !=0)
+//            {
+//                Mat test = new Mat(img);
+//                org.bytedeco.javacpp.opencv_imgcodecs.imwrite(src+"(4)", test);
+//            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
         }
     }
     
